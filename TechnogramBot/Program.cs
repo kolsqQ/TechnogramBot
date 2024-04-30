22b87075ae5575cae1e072b2ed5cb912df1928ae
kolsqQ <kolsq1337@mail.ru>
2024-04-30 09:58:01 +0300

    подключил бота

--- TechnogramBot/Program.cs
+++ TechnogramBot/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PRTelegramBot.Core;
+
+const string zxc = "exit";
+
+var botik = new PRBot(option =>
+{
+    option.Token = "6777113007:AAEFNCFVMHEylnlI-4XNswnOVOGMi0VIp3E";
+    option.ClearUpdatesOnStart = true;
+    option.Admins = new List<long>() { };
+    option.BotId = 0;
+});
+
+
+
+
+
+botik.OnLogCommon += Telegram_OnLogCommon;
+botik.OnLogError += Telegram_OnLogError;
+
+
+await botik.Start();
+
+void Telegram_OnLogError(Exception ex, long? id)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    string errorMsg = $"{DateTime.Now}:{ex}";
+    Console.WriteLine(errorMsg);
+    Console.ResetColor();
+}
+
+void Telegram_OnLogCommon(string msg, Enum typeEvent, ConsoleColor color)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    string message = $"{DateTime.Now}:{msg}";
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if (result.ToLower() == zxc)
+    {
+        Environment.Exit(0);
+    }
+}
+
---
+++ TechnogramBot/main.cs
@@ -0,0 +1,21 @@
+﻿using PRTelegramBot.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TechnogramBot
+{
+    public class main
+    {
+        [ReplyMenuHandler("привет")]
+        public static async Task Commands(ITelegramBotClient botClient, Update update) 
+        {
+            var msg = "иди нахуй";
+            var sendmsg = await PRTelegramBot.Helpers.Message.Send(botClient , update, msg);
+        }
+    }
+}
